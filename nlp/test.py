bc7ef9adfad0d93086b8f9ce1edafb2a1d55eb99
Clanghade <clement.langhade@epitech.eu>
2022-09-09 17:53:08 +0200

    Clang | correct test archi

--- nlp/test.py
+++ nlp/test.py
@@ -1,28 +1,55 @@
-import nlp_perso
+# import nlp_perso
+import requests
 
-assert nlp_perso.test(data="Je souhaite aller à Rennes depuis Paris",
-                      prov="Paris", dest="Rennes"), "Test1 error"
-assert nlp_perso.test(data="Je souhaite partir depuis Paris et aller à Rennes",
-                      prov="Paris", dest="Rennes"), "Test2 error"
-assert nlp_perso.test(data="Je souhaite aller a Rennes en partant de Paris",
-                      prov="Paris", dest="Rennes"), "Test3 error"
-assert nlp_perso.test(data="Je souhaite aller de Rennes à Paris",
-                      prov="Rennes", dest="Paris"), "Test4 error"
-assert nlp_perso.test(data="Je souhaite partir de Paris pour aller à Rennes",
-                      prov="Paris", dest="Rennes"), "Test5 error"
-assert nlp_perso.test(data="Je souhaite aller à Rennes à partir de Paris",
-                      prov="Paris", dest="Rennes"), "Test6 error"
-assert nlp_perso.test(data="Je souhaite faire Paris Rennes",
-                      prov="Paris", dest="Rennes"), "Test7 error"
-assert nlp_perso.test(data="Je souhaite faire Rennes Paris",
-                      prov="Rennes", dest="Paris"), "Test8 error"
-assert nlp_perso.test(data="Je souhaite aller a Rennes en provenance de Paris",
-                      prov="Paris", dest="Rennes"), "Test9 error"
-assert nlp_perso.test(data="Je souhaite prendre un train en provenance de Paris et a destination de Rennes",
-                      prov="Paris", dest="Rennes"), "Test10 error"
-assert nlp_perso.test(data="Je souhaite prendre un train a destination de Rennes et en provenance de Paris",
-                      prov="Paris", dest="Rennes"), "Test11 error"
-assert nlp_perso.test(data="Je souhaite un hamburger",
-                      prov="Paris", dest="Rennes") == False, "Test12 error"
-assert nlp_perso.test(data="",
-                      prov="", dest="") == False, "Test13 error"
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite aller à Rennes depuis Paris"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test1"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite partir depuis Paris et aller à Rennes"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test1"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite aller a Rennes en partant de Paris"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test1"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite aller de Rennes à Paris"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Rennes", "Wrong departure for test1"
+assert r.json()["Destination"] == "Paris", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite partir de Paris pour aller à Rennes"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test1"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite aller à Rennes à partir de Paris"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test1"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite faire Paris Rennes"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test1"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite faire Rennes Paris"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Rennes"] == "Paris", "Wrong departure for test1"
+assert r.json()["Paris"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite prendre un train en provenance de Paris et a destination de Rennes"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Rennes"] == "Paris", "Wrong departure for test1"
+assert r.json()["Paris"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", "Je souhaite prendre un train a destination de Rennes et en provenance de Paris"})
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["Rennes"] == "Paris", "Wrong departure for test1"
+assert r.json()["Paris"] == "Rennes", "Wrong destination for test1"
+
+r = requests.post("http://localhost:8989/trip", json={"data", ""})
+assert r.status_code == 400, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
