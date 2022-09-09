87d9b40fef429262f414ee0819e9a18cfba0a0cd
Clanghade <clement.langhade@epitech.eu>
2022-09-09 13:13:12 +0200

    Clang | remove qt, replace by mp3

--- speechtotext/tests/tests.py
+++ speechtotext/tests/tests.py
@@ -5,7 +5,7 @@ r = requests.post("http://localhost:5000/speechtotext", files=files)
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
 assert r.json()["data"].lower() == "je veux aller a paris en passant par nantes depuis rennes"
 
-files=[('filedata',('3_Allee_Raymond_Cornon.qt',open('./3_Allee_Raymond_Cornon.qt','rb')))]
+files=[('filedata',('pariscarcassone.mp3',open('./pariscarcassone.mp3','rb')))]
 r = requests.post("http://localhost:5000/speechtotext", files=files)
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
-assert r.json()["data"].lower() == "je veux aller a paris en passant par nantes depuis rennes"
+assert r.json()["data"].lower() == "je cherche un train de paris à carcassonne"
