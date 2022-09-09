2d2228fe89b515e1553e09be6679a9784f4f031b
Clanghade <clement.langhade@epitech.eu>
2022-09-09 13:25:56 +0200

    Clang | test

--- speechtotext/app.py
+++ speechtotext/app.py
@@ -4,14 +4,17 @@ import flask_cors
 import unidecode
 import os
 from pydub import AudioSegment
+import subprocess
+
 
 app = flask.Flask(__name__)
 flask_cors.CORS(app, resources={r'/*': {'origins': '*'}})
 
 def convert(rawFile):
     rawFile.save(rawFile.filename)
-    audSeg = AudioSegment.from_file(rawFile.filename)
-    audSeg.export(f"file.wav", format="wav")
+    # audSeg = AudioSegment.from_file(rawFile.filename)
+    # audSeg.export(f"file.wav", format="wav")
+    subprocess.call(['ffmpeg', '-i', rawFile.filename, 'file.wav'])
     os.remove(rawFile.filename)
 
 @app.route('/speechtotext', methods=["POST"])
