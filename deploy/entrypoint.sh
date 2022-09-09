8d1f274bedc4e8ca7e212bbdb8e02463d06b70e7
Clanghade <clement.langhade@epitech.eu>
2022-09-09 15:00:41 +0200

    Clang | correct deployement

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 export GITHUB="true"
 
 /bin/drone-ssh --username ubuntu --password QAYxupCRSqkf --host 51.255.173.25 --script "cd T-AIA-901-REN_1/ &&
-                                                                                        git pull origin master &&
+                                                                                        git pull origin main &&
                                                                                         docker-compose down &&
                                                                                         docker-compose build &&
                                                                                         docker-compose up -d"
