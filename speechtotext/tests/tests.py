1766a5c7121f4a47a06eab99f95012a5408969ca
Clanghade <clement.langhade@epitech.eu>
2022-09-09 13:19:05 +0200

    Clang | add another .wav

--- speechtotext/tests/tests.py
+++ speechtotext/tests/tests.py
@@ -1,11 +1,16 @@
 import requests
 
-files=[('filedata',('3_Allee_Raymond_Cornon.wav',open('./3_Allee_Raymond_Cornon.wav','rb')))]
+files=[('filedata',('3_Allee_Raymond_Cornon.wav', open('./3_Allee_Raymond_Cornon.wav','rb')))]
 r = requests.post("http://localhost:5000/speechtotext", files=files)
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
 assert r.json()["data"].lower() == "je veux aller a paris en passant par nantes depuis rennes"
 
-files=[('filedata',('pariscarcassone.mp3',open('./pariscarcassone.mp3','rb')))]
+files=[('filedata',('pariscarcassone.wav', open('./pariscarcassone.wav','rb')))]
+r = requests.post("http://localhost:5000/speechtotext", files=files)
+assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
+assert r.json()["data"].lower() == "je cherche un train de paris à carcassonne"
+
+files=[('filedata',('pariscarcassone.mp3', open('./pariscarcassone.mp3','rb')))]
 r = requests.post("http://localhost:5000/speechtotext", files=files)
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
 assert r.json()["data"].lower() == "je cherche un train de paris à carcassonne"
