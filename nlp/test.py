02cfab4a8f1a19fbcd58adb4dd5d8ec83bd83b13
Clanghade <clement.langhade@epitech.eu>
2022-09-09 18:02:47 +0200

    Clang | correct test archi

--- nlp/test.py
+++ nlp/test.py
@@ -38,18 +38,18 @@ assert r.json()["Destination"] == "Rennes", "Wrong destination for test8"
 
 r = requests.post("http://localhost:8989/trip", json={"data" : "Je souhaite faire Rennes Paris"})
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
-assert r.json()["Rennes"] == "Paris", "Wrong departure for test9"
-assert r.json()["Paris"] == "Rennes", "Wrong destination for test9"
+assert r.json()["Departure"] == "Rennes", "Wrong departure for test9"
+assert r.json()["Destination"] == "Paris", "Wrong destination for test9"
 
 r = requests.post("http://localhost:8989/trip", json={"data" : "Je souhaite prendre un train en provenance de Paris et a destination de Rennes"})
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
-assert r.json()["Rennes"] == "Paris", "Wrong departure for test10"
-assert r.json()["Paris"] == "Rennes", "Wrong destination for test10"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test10"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test10"
 
 r = requests.post("http://localhost:8989/trip", json={"data" : "Je souhaite prendre un train a destination de Rennes et en provenance de Paris"})
 assert r.status_code == 200, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
-assert r.json()["Rennes"] == "Paris", "Wrong departure for test11"
-assert r.json()["Paris"] == "Rennes", "Wrong destination for test11"
+assert r.json()["Departure"] == "Paris", "Wrong departure for test11"
+assert r.json()["Destination"] == "Rennes", "Wrong destination for test11"
 
 r = requests.post("http://localhost:8989/trip", json={"data" : ""})
 assert r.status_code == 400, f"STATUS CODE ERROR : {r.status_code}, content : {r.content}"
