eafcd959472a1a80271a30f166f8f457f5f8413e
Clanghade <clement.langhade@epitech.eu>
2023-01-27 14:16:33 +0100

    Clang | correct workflow

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,11 +4,11 @@ set -eu
 
 export GITHUB="true"
 
-/bin/drone-ssh --username ubuntu --password QAYxupCRSqkf --host 51.255.173.25 --script "cd T-AIA-901-REN_1/ &&
-                                                                                        git pull origin main &&
-                                                                                        docker-compose down &&
-                                                                                        docker-compose build &&
-                                                                                        docker-compose up -d &&
-                                                                                        sleep 2 &&
-                                                                                        curl localhost:8000 &&
-                                                                                        python3 add_column.py"
+/bin/drone-ssh --username ubuntu --command.timeout 100m0s --password QAYxupCRSqkf --host 51.255.173.25 --script "cd T-AIA-901-REN_1/ &&
+                                                                                                                 git pull origin main &&
+                                                                                                                 docker-compose down &&
+                                                                                                                 docker-compose build &&
+                                                                                                                 docker-compose up -d &&
+                                                                                                                 sleep 2 &&
+                                                                                                                 curl localhost:8000 &&
+                                                                                                                 python3 add_column.py"
